c2840bb66788b525996bdf8473d6eb1011232559
Frank Gerold <FrankETSGerold@gmail.com>
2020-05-02 17:19:53 -0400

    Delete task&user, also separate routes into router files

---
+++ src/routes/taskRouter.js
@@ -0,0 +1,128 @@
+const express = require('express')
+
+const Task = require('../models/task')
+
+const router = new express.Router()
+
+// Task routes
+
+router.get('/tasks', async (req, res) => {
+  try {
+    let tasks = await Task.find({})
+
+    res.send(tasks)
+  } catch (e) {
+    res.status(500)
+    .send(e)
+  }
+})
+// app.get('/tasks', (req, res) => {
+//   Task.find({})
+//   .then(tasks=>res.send(tasks))
+//   .catch(e=>res.status(500).send(e))
+// })
+
+router.get('/tasks/:id', async (req, res) => {
+  let id = req.params.id
+
+  try {
+    let task = await Task.findById(id)
+
+    if (!task) {
+      return res.status(404).send('Task not found')
+    }
+
+    res.send(task)
+  } catch (e) {
+    res.status(500)
+    .send(e)
+  }
+})
+// app.get('/tasks/:id', (req, res) => {
+//   let id = req.params.id
+//   Task.findById(id)
+//   .then(task => {
+//     if (!task) {
+//       return res.status(404).send('Task not found')
+//     }
+//
+//     res.send(task)
+//   })
+//   .catch(e=>res.status(500).send(e))
+// })
+
+router.post('/tasks', async (req, res) => {
+  let taskParams = new Task(req.body)
+  try {
+    let newTask = await taskParams.save()
+
+    res.status(201)
+    .send(newTask)
+  } catch (e) {
+    res.status(400)
+    .send(e)
+  }
+})
+// app.post('/tasks', (req, res) => {
+//   new Task(req.body)
+//   .save()
+//   .then(task=>res.status(201).send(task))
+//   .catch(e => {
+//     res.status(400)
+//     .send(e)
+//   })
+// })
+
+// Update Task
+router.patch('/tasks/:id', async (req, res) => {
+  let id = req.params.id
+  let taskUpdate = req.body
+
+  // Param Validation:
+  const allowedUpdates = ['title', 'description', 'complete']
+  let updates = Object.keys(taskUpdate)
+
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    res.status(400)
+    .send('Invalid task updates')
+  }
+
+  try {
+    let task = await Task.findByIdAndUpdate(id, taskUpdate, {new: true, runValidators: true})
+
+    if (!task) {
+      return res.status(404).send('Task not found')
+    }
+
+    res.send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+// Delete a task
+router.delete('/tasks/:id', async (req, res) => {
+  // Grab desired task ID from uri params
+  let taskId = req.params.id
+
+  try {
+    // Try to delete the task
+    let task = await Task.findByIdAndDelete(taskId)
+
+    // If DB search doesnt find given task, return an error message
+    if (!task) {
+      return res.status(404).send('Task not found...')
+    }
+
+    // Send back the deleted task if successfully removed from DB
+    res.send(task)
+  } catch (e) {
+    // Send a server error message if a backend issue prevents the
+    // operation
+    res.status(500).send(e)
+  }
+})
+
+module.exports = router;
---
+++ src/routes/userRouter.js
@@ -0,0 +1,142 @@
+const express = require('express')
+
+const User = require('../models/user')
+
+const router = new express.Router()
+
+// userRouter.get('/test', (req, res) => {
+//   res.send('from nerw file')
+// })
+
+// /////////////////////////////////////////
+// User routes
+
+  // app.get('/users', (req, res) => {
+  //   User.find({})
+  //   .then(users=>res.send(users))
+  //   .catch(e => {
+  //     res.status(500)
+  //     .send(/*e*/)
+  //   })
+  // })
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({})
+    res.send(users)
+  } catch (e) {
+    res.status(500)
+    .send(e)
+  }
+})
+
+// app.get('/users/:id', (req, res) => {
+//   let _id = req.params.id
+//
+//   User.findById(_id)
+//   .then(user => {
+//     if (!user) {
+//       return res.status(404).send('user not found')
+//     }
+//
+//     res.send(user)
+//   })
+//   .catch(e => res.status(500).send())
+// })
+router.get('/users/:id', async (req, res) => {
+  let id = req.params.id
+
+  try {
+    let user = await User.findById(id)
+
+    if (!user) {
+      return res.status(404)
+      .send('user not found')
+    }
+
+    res.send(user)
+
+  } catch (e) {
+    res.status(500)
+    .send(e)
+  }
+})
+
+// app.post('/users', (req, res) => {
+//   let newUser = new User(req.body)
+//
+//   newUser.save()
+//   .then(() => res.status(201).send(newUser))
+//   .catch(e => {
+//     res.status(400)
+//     .send(e)
+//   })
+// })
+router.post('/users', async (req, res) => {
+  let userParams = new User(req.body)
+
+  try {
+    let newUser = await userParams.save()
+    res.status(201)
+    .send(newUser)
+
+  } catch (e) {
+    res.status(400)
+    .send(e)
+  }
+})
+
+router.patch('/users/:id', async (req, res) => {
+  let id = req.params.id
+  let updateParams = req.body
+
+  // validate update fields
+  let attemptedUpdates = Object.keys(updateParams)
+  const allowedUpdates = ['name', 'email', 'password', 'age']
+// every tests if all elements in an array pass the test
+  const isValidOperation = attemptedUpdates.every(update => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400)
+    .send({
+      error: 'Invalid Update'
+    })
+  }
+
+  // Attempt to update user
+  try {
+    let updatedUser = await User.findByIdAndUpdate(id, updateParams, {
+      new: true,
+      runValidators: true
+    })
+
+    if (!updatedUser) {
+      return res.status(404)
+      .send('user not found')
+    }
+
+    res.send(updatedUser)
+  } catch (e) {
+    res.status(400)
+    .send(e)
+  }
+})
+
+// Delete User
+router.delete('/users/:id', async (req, res) => {
+  let id = req.params.id
+
+  try {
+    let user = await User.findByIdAndDelete(id)
+
+    if (!user) {
+      return res.status(404).send('user not found')
+    }
+
+    res.send(user)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
+
+module.exports = router;
