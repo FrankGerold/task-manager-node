ed43f60f5380f9e0d0bbda5eb896c4ebdc80782d
Frank Gerold <FrankETSGerold@gmail.com>
2020-05-02 21:49:42 -0400

    Jwt auth middleware created

---
+++ src/middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const auth = async (req, res, next) => {
+  // Header: Authorization: bearer token
+  try {
+    const token = req.header('Authorization').split(" ")[1]
+    // or token.replace('Bearer ', '')
+
+    const decoded = jwt.verify(token, 'Super Secret Key Alert!')
+
+    const user = await User.findOne({
+      _id: decoded._id,
+      'tokens.token': token
+    })
+
+    if (!user) {
+      throw new Error()
+    }
+
+    req.user = user
+    next()
+  } catch (e) {
+    res.status(401)
+    .send({error: "Please authenticate!"})
+  }
+}
+
+module.exports = auth;
